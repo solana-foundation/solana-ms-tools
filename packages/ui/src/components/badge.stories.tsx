d799bad7f8724a27a5dcb3a262bde7392865413e
Sergo <rogaldh@radsh.red>
2025-05-22 14:18:22 +0100

    Feature/update theme (#12)
    * feat: update components
    
    * feat: update theme

--- packages/ui/src/components/badge.stories.tsx
+++ packages/ui/src/components/badge.stories.tsx
@@ -4,7 +4,7 @@ import { Badge } from './badge'
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Components/Badge',
+  title: 'Badge',
   component: Badge,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout

5a32ef22c5de4e71acaa692b0e3bc2742745a6d7
nchigryay <natalia.chigryay@hoodies.team>
2025-06-04 16:22:43 +0100

    Rm storybook eslint config

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,10 +1,7 @@
 import { configs } from '@solana-foundation/ms-tools-config/eslint.config.react.mjs'
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from 'eslint-plugin-storybook'
 import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  storybook,
   configs.react,
   {
     languageOptions: {
